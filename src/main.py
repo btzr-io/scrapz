3bd165974f8f3a3b67f1b95f7d2e293df5a6cdfc
btzr-io <btzr.io@gmail.com>
2020-05-13 00:46:08 -0500

    add inital vars for testing

--- src/main.py
+++ src/main.py
@@ -1,1 +1,11 @@
-# Init
+from typing import Final
+
+#--------------------- #
+#  Constant variables  #
+# -------------------- #
+
+# Public chainquery API
+CHAINQUERY_API: Final = "https://chainquery.lbry.io/api/sql?query=";
+
+# The mature tag is the first entry on the tags table
+MATURE_TAG_ID: Final = 1;
