13ed57bf11c948da5cdcc96fe4441aad23703fc2
btzr-io <btzr.io@gmail.com>
2020-05-13 20:06:39 -0500

    initial chainquery python wrapper

---
+++ src/chainquery/__init__.py
@@ -0,0 +1,60 @@
+#----------------- #
+#  Import modules  #
+# ---------------- #
+
+import requests as req
+from typing import Final
+from pypika import Query, Table
+
+#--------------------- #
+#  Constant variables  #
+# -------------------- #
+
+# Public chainquery API endpoints:
+CHAINQUERY_API: Final = "https://chainquery.lbry.io/api";
+CHAINQUERY_API_SQL: Final = CHAINQUERY_API + "/sql";
+
+# The mature tag is the first entry on the tags table
+MATURE_TAG_ID: Final = 1;
+
+#-------------- #
+#  Test query   #
+# ------------- #
+
+def testQuery():
+    claim = Table('claim')
+    # Basic query
+    q =  Query.from_(claim).select(claim.name, claim.claim_id).limit(1000)
+    # returns string from query
+    return q.get_sql().replace('"', '')
+
+#--------------------- #
+#  Chainqury wrapper   #
+# -------------------- #
+
+def query():
+    try:
+        # Build query string
+        q = testQuery()
+        # Send the sql query as url parameter
+        payload = {'query': q }
+
+        # Initial request test
+        res = req.get(CHAINQUERY_API_SQL, params = payload)
+
+        res.raise_for_status()
+
+        # Parse to json
+        res = res.json()
+
+        data = res['data']
+
+        print(data)
+
+    # Handle connection error
+    except req.ConnectionError as error:
+        print("[!] Connection error: {0}".format(error))
+
+    # Handle http error ( 404, 503, etc... )
+    except req.HTTPError as error:
+        print("[!] Http error: {0}".format(error))
--- src/main.py
+++ src/main.py
@@ -2,44 +2,7 @@
 #  Import modules  #
 # ---------------- #
 
-# Http / https requests
-import requests
+import chainquery as chain
 
-# Type checking
-from typing import Final
-
-#--------------------- #
-#  Constant variables  #
-# -------------------- #
-
-# Public chainquery API
-CHAINQUERY_API: Final = "https://chainquery.lbry.io/api";
-CHAINQUERY_API_SQL: Final = CHAINQUERY_API + "/sql";
-
-# The mature tag is the first entry on the tags table
-MATURE_TAG_ID: Final = 1;
-
-#--------------- #
-#  Inital test   #
-# -------------- #
-
-try:
-    # Send the sql query as url parameter
-    payload = {'query': ''}
-
-    # Initial request test
-    response = requests.get(CHAINQUERY_API)
-
-    # Parse to json
-    response.raise_for_status()
-
-    # Print to console
-    print(response.json())
-
-# Handle connection error
-except requests.ConnectionError as error:
-     print("[!] Connection error: {0}".format(error))
-
-# Handle http error ( 404, 503, etc... )
-except requests.HTTPError as error:
-    print("[!] Http error: {0}".format(error))
+# test...
+chain.query()
